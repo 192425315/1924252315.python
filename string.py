330eb154e6d2058367871be14d4646be9269ae75
192425315 <sravanthiduggirala1124@gmail.com>
2025-06-19 10:29:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+STRING
+
+
+s="good morning"
+print(s[2])
+print(s[6])
+print(s[2:])
+print(s[:4])
+print(s+"friends")
+print(s*2)
+"m" in s
+"a" not in s
---
+++ traffic light system.py
@@ -0,0 +1,7 @@
+colour=input("enter the colour of light:")
+if(colour=="green"):
+    print("go")
+elif(colour=="yellow"):
+    print("get ready")
+else:
+    print("stop")
