881e647a847b7e25f1b1ff55c3e83716e5d2f2ea
192425315 <sravanthiduggirala1124@gmail.com>
2025-06-20 15:26:17 +0530

    8.series and patterns.py

--- 8.series and patterns.py
+++ 8.series and patterns.py
@@ -1,13 +1,8 @@
-# Number series: 1 2 3 ... n
 n = 5
 for i in range(1, n+1):
     print(i, end=' ')
 print()
-
-# Number Pattern
 for i in range(1, n+1):
     print(str(i) * i)
-
-# Pyramid pattern
 for i in range(1, n+1):
     print(' ' * (n - i) + '* ' * i)
