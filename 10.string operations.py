4272800f1b873a5d91790920c5a944fec56cc657
192425315 <sravanthiduggirala1124@gmail.com>
2025-06-20 15:24:33 +0530

    10.string operations.py

--- 10.string operations.py
+++ 10.string operations.py
@@ -1,11 +1,5 @@
 text = "hello world"
-
-# Reverse string
 print("Reversed:", text[::-1])
-
-# Character count
 char = 'l'
 print(f"Count of '{char}':", text.count(char))
-
-# Replacing characters
 print("Replaced string:", text.replace("l", "x"))
